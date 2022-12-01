e6e3ac469cae0e8bc4d5fed72b9d00605f387010
Katherine Reyes <98465512+Kathereyes19@users.noreply.github.com>
2022-12-01 17:18:01 -0500

    Taller Final

---
+++ FINAL/app/components/Home/feed/dataMN.ts
@@ -0,0 +1,25 @@
+export interface DataMenu {
+    logo: string;
+    casa: string;
+    brujula: string;
+    mensajes: string;
+    heart: string;
+    nuevopost: string;
+    perfilfoto: string;
+
+  }
+  
+  const dataMN: DataMenu[] = [
+    {
+        "logo": "./components/Home/feed/components/profile/img/logo.png",
+        "casa": "./components/Home/feed/components/profile/img/casa.png",
+        "brujula": "./components/Home/feed/components/profile/img/brujula.png",
+        "mensajes": "./components/Home/feed/components/profile/img/mensajes.png",
+        "heart": "./components/Home/feed/components/profile/img/heart.png",
+        "nuevopost": "./components/Home/feed/components/profile/img/nuevo.png",
+        "perfilfoto": "https://images.pexels.com/photos/13625508/pexels-photo-13625508.jpeg?cs=srgb&dl=pexels-alteredsnaps-13625508.jpg&fm=jpg",
+
+    },
+
+  ]
+  export default dataMN;
---
+++ FINAL/app/index.ts
@@ -0,0 +1,82 @@
+import "./components/index.js";
+enum Screens {
+    login,
+    register,
+    home,
+    newpost
+}
+
+enum HMAttributes {
+    'onNewPost' = 'onNewPost'
+}
+
+interface HMProperties extends Element {
+    onNewPost: (val:EventListener) => void;
+}
+class AppContainer extends HTMLElement{
+    screen: Screens = Screens.home;
+
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback(){
+        this.render();
+
+        this.setEventListeners();
+    }
+
+    setEventListeners() {
+        const Home = () => this.changeScreen(Screens.home);
+        const Login = () => this.changeScreen(Screens.login);
+        const Register = () => this.changeScreen(Screens.register);
+        const NewPost = () => this.changeScreen(Screens.newpost);
+
+        const newpost = this.shadowRoot?.querySelector("app-newpost");
+        newpost?.addEventListener('form-sucess', Home);
+
+        const home = this.shadowRoot?.querySelector("app-home");
+        (home as HMProperties | undefined)?.onNewPost(NewPost);
+
+        const login = this.shadowRoot?.querySelector("app-login");
+        login?.addEventListener("login-success", Home);
+        login?.addEventListener('go-register', Register);
+        
+        const register = this.shadowRoot?.querySelector("app-register");
+        register?.addEventListener("login-success", Login);
+        register?.addEventListener('go-login', Login);
+    }
+
+    render(){
+        if(!this.shadowRoot) return;
+        switch (this.screen) {
+            case Screens.home:
+                this.shadowRoot.innerHTML = "<app-home></app-home>"
+                break;
+        
+            case Screens.login:
+                this.shadowRoot.innerHTML = "<app-login></app-login>"
+            break;
+            
+            case Screens.register:
+                this.shadowRoot.innerHTML = "<app-register></app-register>"
+            break;
+
+            case Screens.newpost: 
+                this.shadowRoot.innerHTML = "<app-newpost></app-newpost>"
+            break;
+
+            default:
+                break;
+        }
+    }
+
+    changeScreen(screen: Screens) {
+        this.screen = screen;
+        this.render();
+        this.setEventListeners();
+    }
+}
+
+customElements.define("app-container",AppContainer);
